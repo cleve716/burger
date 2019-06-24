11ef180b090d8aff7ed1814d612cf009e0cbe3d0
Cleve716 <cleve716@msn.com>
2019-06-23 18:13:48 -0600

    added db schema and seeds, and set burgers_controller

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Booger Burger');
+INSERT INTO burgers (burger_name) VALUES ('Impossible Burger');
+INSERT INTO burgers (burger_name) VALUES ('Burger Burger');
